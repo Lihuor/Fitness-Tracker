9a3883b2e9acbf7c6953205f96d3f1e54613c480
Lihuor <lihuor@gmail.com>
2020-09-16 00:28:32 +1000

    fixed links in HTML

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const express = require('express');
+const logger = require('morgan');
 const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-    useNewUrlParser: true,
-    useFindAndModify: false
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fittrack", {useNewUrlParser: true});
+
 
 // routes
 app.use(require("./routes/api-routes"));
