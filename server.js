e0088259323202c40a77ac3bf1bc671b2f2da7f2
Lihuor <lihuor@gmail.com>
2020-09-16 19:48:07 +1000

    fixed routes

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
+//install packages
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
-
+//set port
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+//use logger
 app.use(logger("dev"));
 
-app.use(express.urlencoded({extended: true}));
+//parser
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//use static files
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fittrack", {useNewUrlParser: true});
 
 
-// routes
-app.use(require("./routes/api-routes"));
-app.use(require("./routes/html-routes"));
+//use routes
+require('./routes/api-routes')(app)
+require('./routes/html-routes')(app)
+
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-});
+    console.log(`App running on port ${PORT}..`);
+})
